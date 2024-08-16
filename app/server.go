bee6f48b0991ecb8366275c5ddb16c1d5ef6be23
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-16 11:45:42 +0700

    add info command

--- app/server.go
+++ app/server.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 	port := flag.String("port", "6379", "Port to connect to")
 	flag.Parse()
-	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%v", port))
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%v", *port))
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
