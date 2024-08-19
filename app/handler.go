2ff8b22e90afe85d2ff1a4faddb44a6e72dcc9c8
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-19 15:22:45 +0700

    pass 1st stage

--- app/handler.go
+++ app/handler.go
@@ -84,7 +84,7 @@ func (s Server) handlecommand(args [][]byte) {
 		if err != nil {
 			fmt.Println("Error decoding", err)
 		}
-		s.writeData(fmt.Sprintf("$%d\r\n%v", len(emptyRDBStr), emptyRDBStr))
+		s.writeData(fmt.Sprintf("$%d\r\n%s", len(emptyRDBStr), []byte(emptyRDBStr)))
 
 	default:
 		s.writeData(simpleStringResponse("unknown"))
