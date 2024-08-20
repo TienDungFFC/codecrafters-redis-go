a2182fe558cd18823b147d12e3337ed6d5a321f0
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-20 20:56:17 +0700

    pass 1st stage

--- app/handler.go
+++ app/handler.go
@@ -123,6 +123,7 @@ func (s *Server) handlecommand(args [][]byte) {
 			go func() {
 				(*slave).Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n"))
 			}()
+			time.Sleep(1 * time.Second)
 		}
 
 		timer := time.After(time.Duration(duration) * time.Millisecond)
