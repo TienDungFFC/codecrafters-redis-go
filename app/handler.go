e0cb3db7e5b604593e5f32af0e9704e43abe7b27
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-20 21:16:02 +0700

    pass 1st stage

--- app/handler.go
+++ app/handler.go
@@ -101,6 +101,7 @@ func (s *Server) handlecommand(args [][]byte) {
 			s.writeData(s.replConfResponse())
 			s.offset += len(s.cmd.Raw)
 		} else if strings.ToLower(string(args[1])) == "ack" {
+			fmt.Println("inside ack:", string(args[1]))
 			s.ackChan <- true
 		} else {
 			s.writeData(simpleStringResponse("OK"))
