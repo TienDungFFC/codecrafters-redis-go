705b3303c952ac0a147571a345db431e3d161ed5
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-20 13:59:34 +0700

    pass 1st stage

--- app/handler.go
+++ app/handler.go
@@ -32,6 +32,7 @@ var mSet = make(map[string]Value)
 
 func (s *Server) handler(str []byte) {
 	args, _ := readCommand(str)
+	fmt.Println("args when getting: ", args)
 	s.cmd.Args = args
 	s.cmd.Raw = str
 	if len(args) == 0 {
@@ -68,7 +69,6 @@ func (s *Server) handlecommand(args [][]byte) {
 			}
 		}
 	case GET:
-		fmt.Println("set after get: ", mSet)
 		val, ok := mSet[string(args[1])]
 		if ok && (val.px.IsZero() || time.Now().Before(val.px)) {
 			s.writeData(bulkStringResponse(strings.TrimSpace(string(val.val))))
