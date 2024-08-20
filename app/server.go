1f61de4554f20fccb475270b103f3c9da52e4de1
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-20 22:07:24 +0700

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -63,9 +63,7 @@ func main() {
 	}
 	if role == SLAVE {
 		rep := strings.Split(*replicaof, " ")
-		lock.Lock()
 		conn, err := connectMaster(rep[0], rep[1])
-		lock.Unlock()
 		server := NewServer(conn, role)
 		if err != nil {
 			fmt.Println(err)
