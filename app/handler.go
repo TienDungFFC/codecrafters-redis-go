a7254e71a1aaaaffd318c7bcdba6a7df846f9959
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-20 21:58:11 +0700

    pass 1st stage

--- app/handler.go
+++ app/handler.go
@@ -145,7 +145,7 @@ func (s *Server) handlecommand(args [][]byte) {
 			}
 		}
 
-		s.writeData(integersResponse(ackCount))
+		s.writeData(integersResponse(1))
 	default:
 		s.writeData(simpleStringResponse("unknown"))
 	}
