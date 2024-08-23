973f1b096687c3cf6e36d5327a425fbeb1fbb93b
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-08-23 16:06:11 +0700

    read multiple keys

--- app/server.go
+++ app/server.go
@@ -27,6 +27,17 @@ func main() {
 	r := RDB{}
 	r.LoadFile()
 	if r.file != nil {
+		b, err := os.ReadFile(_metaInfo.dir + "/" + _metaInfo.dbFileName) // just pass the file name
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		fmt.Println(b) // print the content as 'bytes'
+
+		str := string(b) // convert content to a 'string'
+
+		fmt.Println(str) // print the content as a 'string'
+
 		r.ReadDB()
 		r.file.Close()
 	} else {
